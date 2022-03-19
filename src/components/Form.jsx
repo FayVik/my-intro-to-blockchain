aa7d11fca786448a5fbc7a4d72c1e9c8a2e234c4
FayVik <vivianemma97@gmail.com>
2022-03-19 21:45:44 +0100

    add responsive to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,41 +7,42 @@ import { ButtonNav } from './Button';
 const Form = () => {
 	return (
 		<div>
-			<form className={`${Style.formContainer} rounded-lg p-4`}>
-				<div className='mb-6'>
-					<input
-						type='text'
-						placeholder='Enter your name'
-						className='rounded'
-					/>
-				</div>
-				<div className='mb-6'>
-					<input
-						type='text'
-						placeholder='Enter title of fund request'
-						className='rounded'
-					/>
-				</div>
-				<div className='mb-6'>
-					<input type='text' placeholder='Enter Amount' className='rounded' />
-				</div>
-
-				<textarea
-					name=''
-					id=''
-					cols='30'
-					rows='5'
-					className='rounded'
-					placeholder='Enter Description'></textarea>
-
-				<div className='mb-6'>
-					<span className={`${Style.spanForm}`}>
-						Upload fund request image/proof
-					</span>
-					<input type='file' className='rounded' />
-				</div>
-				<div className='m'>
-					<ButtonNav>Submit</ButtonNav>
+			<form>
+				<div
+					className={`${Style.formContainer} rounded-lg p-4 lg:w-2/5 md:w-full items-center flex flex-wrap`}>
+					<div className='mb-4 w-full'>
+						<input
+							type='text'
+							placeholder='Enter your name'
+							className='rounded'
+						/>
+					</div>
+					<div className='mb-4 w-full'>
+						<input
+							type='text'
+							placeholder='Enter title of fund request'
+							className='rounded'
+						/>
+					</div>
+					<div className='mb-4 w-full'>
+						<input type='text' placeholder='Enter Amount' className='rounded' />
+					</div>
+					<textarea
+						name=''
+						id=''
+						cols='30'
+						rows='5'
+						className='rounded w-full'
+						placeholder='Enter Description'></textarea>
+					<div className='mb-4 w-full'>
+						<span className={`${Style.spanForm}`}>
+							Upload fund request image/proof
+						</span>
+						<input type='file' className='rounded' />
+					</div>
+					<div className='w-full'>
+						<ButtonNav>Submit</ButtonNav>
+					</div>
 				</div>
 			</form>
 		</div>
